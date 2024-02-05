11cbbf04c8afdd2c0eefe8882a97a29a7abf11c1
Audrey Castillo <valax321@gmail.com>
2024-02-05 20:28:32 +1100

    Fix batch file God, batch files suck eh

--- make_release_win64.cmd
+++ make_release_win64.cmd
@@ -1,10 +1,14 @@
 @echo off
 
+setlocal ENABLEDELAYEDEXPANSION
+
 REM If we're doing a CI build, then do a development build instead
 set BUILD_TYPE="win64-release"
 if %RADISH_CI_BUILD%=="1" (
     set BUILD_TYPE="win64-development"
 )
+
+echo Build type: %BUILD_TYPE%
 
 cmake --preset %BUILD_TYPE% || exit 1
 cmake --build --preset %BUILD_TYPE% --target RadishRuntime || exit 1
