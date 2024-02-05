93629daa7aa712f93edf7b05341474e85e5df668
Audrey Castillo <valax321@gmail.com>
2024-02-05 20:31:23 +1100

    BATCH. FILES.

--- make_release_win64.cmd
+++ make_release_win64.cmd
@@ -3,12 +3,11 @@
 setlocal ENABLEDELAYEDEXPANSION
 
 REM If we're doing a CI build, then do a development build instead
-set BUILD_TYPE="win64-release"
 if %RADISH_CI_BUILD%=="1" (
     set BUILD_TYPE="win64-development"
+) else (
+    set BUILD_TYPE="win64-release"
 )
-
-echo Build type: %BUILD_TYPE%
 
 cmake --preset %BUILD_TYPE% || exit 1
 cmake --build --preset %BUILD_TYPE% --target RadishRuntime || exit 1
