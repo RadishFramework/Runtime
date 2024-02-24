b7db35eeb9d4fc1833048158503e55926953aae2
Audrey Castillo <valax321@gmail.com>
2024-02-24 13:10:58 +1100

    Actually added quickjs??

--- src/radish/private/vm.cpp
+++ src/radish/private/vm.cpp
@@ -1,21 +1,31 @@
 #include "radish/vm.hpp"
+#include "radish/logging.hpp"
 
-using namespace hermes;
+#include "quickjs/quickjs.h"
 
 namespace radish {
 
+// 1GB
+constexpr size_t DEFAULT_MEMORY_LIMIT = 1073741824;
+
 virtual_machine::create_error virtual_machine::create() {
 
-    auto cfg = vm::RuntimeConfig::Builder()
-        .withEnableEval(false)
-        .build();
-
-    auto runtime = fbhm::makeHermesRuntime(cfg);
+    auto runtime = JS_NewRuntime();
+    if (!runtime) {
+        return {"Failed to allocate runtime memory"};
+    }
 
     auto vm = std::unique_ptr<virtual_machine>(new virtual_machine());
-    vm->_runtime = std::move(runtime);
+    vm->_runtime = runtime;
 
     return create_error{std::move(vm)};
 }
 
+virtual_machine::~virtual_machine() {
+    if (_runtime != nullptr) {
+        JS_FreeRuntime(_runtime);
+        _runtime = nullptr; 
+    }
 }
+
+} // namespace radish
--- src/radish/public/radish/vm.hpp
+++ src/radish/public/radish/vm.hpp
@@ -3,20 +3,21 @@
 #include "radish.hpp"
 #include "radish/error.hpp"
 
-#include "hermes/hermes.h"
+struct JSRuntime;
+struct JSContext;
 
 namespace radish {
-
-namespace fbhm = facebook::hermes;
 
 class virtual_machine {
 public:
     using create_error = error<std::unique_ptr<virtual_machine>, std::string>;
 
     static create_error create();
+    virtual ~virtual_machine();
 
 private:
-    std::unique_ptr<fbhm::HermesRuntime> _runtime{};
+    JSRuntime* _runtime{};
+    JSContext* _context{};
 };
 
 }
